3cca8504aad1ae729889c94c725841a48ef76370
AsierGH-ULE <amartt08@estudiantes.unileon.es>
2025-03-19 15:47:43 +0100

    #5 .env added

--- config/db.js
+++ config/db.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const MONGO_URI = 'mongodb://asier:asier@localhost:27017/sensores?authSource=admin';
+const MONGO_URI = process.env.MONGO_CREDENTIALS ||
+    'mongodb://localhost:27017/sensores';
+// URI por defecto: 'mongodb://localhost:27017/sensores'
 const connectDB = async () => {
     try {
         await mongoose.connect(MONGO_URI);
@@ -9,4 +12,4 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
-module.exports = connectDB;  
+module.exports = connectDB; 
